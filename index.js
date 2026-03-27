8108d3ba88427a0487bce00dc93dbe097019dc7c
Jeremi <jeremi_P@op.pl>
2026-03-27 21:30:57 +0100

    Initial backend

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors');
+const projects = require("./projects.json")
+const app = express()
+
+const port = 3000
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://rerender-two.vercel.app',
+]
+
+app.use(cors({
+  origin(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+      return
+    }
+
+    callback(new Error('Not allowed by CORS'))
+  }
+}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/projects", (req, res) => {
+    res.send(projects)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
