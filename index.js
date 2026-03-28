981a39065c4d229fdae9d6ef2e1086ffc93e29fd
Jeremi <jeremi_P@op.pl>
2026-03-28 08:42:21 +0100

    Use hosting port from environment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const projects = require("./projects.json")
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const allowedOrigins = [
   'http://localhost:5173',
