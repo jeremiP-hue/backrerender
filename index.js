3742ccb5b8e5f19206c027d4d80fc7afe4dc6e1d
Jeremi <jeremi_P@op.pl>
2026-04-09 17:39:34 +0200

    zuba baza dadanych ogurkowa

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { createClient } = require('@supabase/supabase-js')
 const express = require('express')
 const cors = require('cors');
 const projects = require("./projects.json")
@@ -9,6 +10,26 @@ const allowedOrigins = [
   'http://localhost:5173',
   'https://rerender-two.vercel.app',
 ]
+const NEXT_PUBLIC_SUPABASE_URL = "https://onriwqfpnzldrvjehhgj.supabase.co"
+const NEXT_PUBLIC_SUPABASE_ANON_KEY = "sb_publishable_vaZTIaMhECO-8qXk-M8SdA_05rdUgJ_"
+
+const supabase = createClient(
+  NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
+
+async function getUsers() {
+  const { data, error } = await supabase
+    .from('projekty')
+    .select('*')
+
+  if (error) {
+    console.error(error)
+    return
+  }
+
+  return (data)
+}
 
 app.use(cors({
   origin(origin, callback) {
@@ -21,13 +42,34 @@ app.use(cors({
   }
 }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  const projekts = await getUsers()
+  res.send(projekts)
+  console.log("test")
 })
 
-app.get("/projects", (req, res) => {
-    res.send(projects)
+app.get("/projects", async (req, res) => {
+  const projekts = await getUsers()
+  res.send(projekts)
 })
+
+app.post("/project", async (req, res) => {
+  const { data, error } = await supabase
+    .from('projekty')
+    .insert([
+      projects[1]
+    ])
+    .select()
+
+
+  if (error) {
+    return res.status(500).json({ error: error.message })
+  }
+
+  res.status(201).json(data)
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
